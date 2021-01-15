3bed095da16cf320790153eee2d6a1373e0e23a7
chahtk <chahtk@gmail.com>
2021-01-15 12:57:34 +0900

    feat: add button component

---
+++ client/src/components/AddButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface StyleProps {
+  left?: boolean;
+  right?: boolean;
+}
+
+interface Props extends StyleProps {
+  handler: () => void;
+}
+
+const AddButtonStyle = styled.button<StyleProps>`
+  width: 20px;
+  height: 20px;
+  border-radius: 20px;
+  outline: none;
+  border: none;
+  position: absolute;
+  left: ${({ left }) => (left ? 0 : null)};
+  right: ${({ right }) => (right ? 0 : null)};
+  opacity: 0.5;
+  &:hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+`;
+
+function AddButton({ handler, left = false, right = false }: Props) {
+  return (
+    <AddButtonStyle onClick={handler} right={right}>
+      +
+    </AddButtonStyle>
+  );
+}
+
+export default AddButton;
